aef54f680c2ad5af77bb82d10d85e4613f3f34a1
edidiongapostle <edidiongapostle@gmail.com>
2025-06-21 13:35:52 +0100

    Modified to fix broken display picture on the about page

--- home/views.py
+++ home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
 
 def about(request):
-    return render(request, 'home/about.html')
-
+    return render(request, 'home/about.html', {'MEDIA_URL': settings.MEDIA_URL})
